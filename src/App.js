fbe026624727648d910c8fec03828a75beea87dd
kyeongbong <kt03170@gmail.com>
2022-04-09 04:12:53 +0900

    Todo and modal UI

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import './App.css';
 import React from 'react';
-
 import {BrowserRouter, Route} from "react-router-dom";
 
-import Card from './components/Card';
 import Header from './components/Header';
 import Modal from './components/Modal';
 import Login from './pages/Login';
 import Singup from './pages/Signup';
 import TodoList from './pages/TodoList';
-import Button from './elements/Button';
-import Grid from './elements/Grid';
-import Input from './elements/Input';
 import star from './elements/star';
 
 
@@ -19,15 +14,11 @@ function App() {
   return (
     <React.Fragment>
       <BrowserRouter>
-        <Route path="/Card" exact component={Card} />
-        <Route path="/Header" exact component={Header} />
-        <Route path="/Modal" exact component={Modal} />
-        <Route path="/Login" exact component={Login} />
-        <Route path="/Singup" exact component={Singup} />
-        <Route path="/TodoList" exact component={TodoList} />
-        <Route path="/Button" exact component={Button} />
-        <Route path="/Grid" exact component={Grid} />
-        <Route path="/Input" exact component={Input} />
+        <Route path="/header" exact component={Header} />
+        <Route path="/modal" exact component={Modal} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/signup" exact component={Singup} />
+        <Route path="/todoList" exact component={TodoList} />
         <Route path="/star" exact component={star} />
       </BrowserRouter>
     </React.Fragment>
