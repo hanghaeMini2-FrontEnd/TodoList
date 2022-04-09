a6f6deb83f587a4b34d59eec86f679b8654e00f2
JIEUNNN <pandaa0628@gmail.com>
2022-04-09 16:50:55 +0900

    스토어 주입

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import React from 'react';
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, Route} from 'react-router-dom';
+import { ConnectedRouter } from 'connected-react-router';
+import { history } from "./redux/configureStore";
 
 import Header from './components/Header';
 import Modal from './components/Modal';
@@ -13,14 +15,14 @@ import star from './elements/star';
 function App() {
   return (
     <React.Fragment>
-      <BrowserRouter>
+      <ConnectedRouter history={history}>
         <Route path="/header" exact component={Header} />
         <Route path="/modal" exact component={Modal} />
         <Route path="/login" exact component={Login} />
         <Route path="/signup" exact component={Singup} />
         <Route path="/todoList" exact component={TodoList} />
         <Route path="/star" exact component={star} />
-      </BrowserRouter>
+      </ConnectedRouter>
     </React.Fragment>
   );
 }
