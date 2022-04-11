f7c066b65e06691573178494c0f2570864b750b5
JIEUNNN <pandaa0628@gmail.com>
2022-04-11 14:30:12 +0900

    회원가입 기능 구현

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,10 +1,20 @@
 import React from "react";
 import styled from "styled-components";
-import Header from "../components/Header";
-import {Grid, Text, Input, Button} from "../elements/Index"
-import { history } from "../redux/configureStore"
+import {Grid, Text, Input, Button} from "../elements/Index";
+import { history } from "../redux/configureStore";
+import { useDispatch } from "react-redux";
+import { actionCreators as userActions } from "../redux/modules/user";
 
 const Singup = (props) => {
+  const dispatch = useDispatch();
+
+  const [id, setId] = React.useState("");
+  const [pwd, setPwd] = React.useState("");
+  const [pwdCheck, setPwdCheck] = React.useState("");
+
+  const signup = () => {
+    dispatch(userActions.signupDB(id, pwd, pwdCheck));
+  }
 
   return (
     <React.Fragment>
@@ -20,7 +30,11 @@ const Singup = (props) => {
             <Input
               type="text"
               label="아이디"
-              placeholder="아이디를 입력하세요."  
+              placeholder="아이디를 입력하세요."
+              _onChange={(e) => {
+                setId(e.target.value);
+                // console.log(e.target.value)
+              }} 
             ></Input>
           </Grid>
           
@@ -29,6 +43,10 @@ const Singup = (props) => {
               type="password"
               label="비밀번호"
               placeholder="비밀번호를 입력하세요."  
+              _onChange={(e) => {
+                setPwd(e.target.value);
+                // console.log(setPwd)
+              }} 
             ></Input>
           </Grid>
           
@@ -37,12 +55,16 @@ const Singup = (props) => {
               type="password"
               label="비밀번호확인"
               placeholder="비밀번호를 다시 입력하세요."  
+              _onChange={(e) => {
+                setPwdCheck(e.target.value);
+                // console.log(setPwd)
+              }} 
             ></Input>
           </Grid>
           
           <Grid margin="20px 0px 0px 0px">
             <Grid margin="10px 0px 0px 0px" padding="0px 16px">
-              <Button bg = "#755139" text="회원가입하기" color = "#F2EDD7" bold = "900"/>
+              <Button bg = "#755139" text="회원가입하기" color = "#F2EDD7" bold = "900" onClick={signup}/>
             </Grid>
             <Grid margin="0px 0px 40px 0px" padding="16px">
               <Button bg = "#755139" color = "#F2EDD7" bold = "900" text="홈으로 가기" onClick={() => {
--- src/redux/modules/user.js
+++ src/redux/modules/user.js
@@ -26,7 +26,7 @@ const loadToken = createAction(LOAD_TOKEN, (user) => ({user}));
 const loginDB = (userId, userPw) => {
   return function (dispatch, getState, {history}) {
     axios
-      .post("DB주소", {
+      .post("", {
         userId: userId,
         userPw: userPw,
       })
@@ -37,22 +37,24 @@ const loginDB = (userId, userPw) => {
 }
 
 // 회원가입 액션
-const signupDB = (userId, userPw) => {
+const signupDB = (userId, userPw, pwCheck) => {
   return function (dispatch, getState, {history}) {
     axios
-    .post("DB주소", {
+    .post("", {
       userId: userId,
       userPw: userPw,
+      pwCheck: pwCheck,
     })
     .then(response => {
       console.log(response);
+      history.push("/login")
     })
     .catch(error => {
       alert("error 메세지 입력");
       console.log("회원가입 DB Error", error);
     })
-  }
-}
+  };
+};
 
 // 토큰 받기 액션
 
